cbdaa20bca43c19569d337da8c531e6a65ad10b7
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-03-30 14:44:00 +0000

    Added code

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2023 Ben Thomson. All rights reserved.
+//
+// Created by: Ben Thomson
+// Created on: 03/30/2023
+// This file contains the JS functions for index.html
+
+// This function calculates the area of a trapezoid
+function calculateVolume () {
+  // Get the user input
+  let baseLength = parseFloat(document.getElementById("base-length").value);
+  let baseHeight = parseFloat(document.getElementById("base-height").value);
+  let heightOfPyramid = parseFloat(document.getElementById("height-pyramid").value);
+
+  // Perform mathematical equations
+  let volumeOfPyramid = baseLength * baseHeight * heightOfPyramid / 6;
+
+  // Display calculations back to user
+  document.getElementById("calculations").innerHTML = "The volume of the right triangular pyramid is " + volumeOfPyramid.toFixed(2) + " cm<sup>3</sup>."
+}
